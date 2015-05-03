cc40afb93f92f54bc429449277842c2cd958856f
Dan Pristupov <danil@danil.cz>
2015-05-03 14:34:18 +0200

    Adds performance measurements using SimpleSpeedTester.

--- src/SkipList.Core.Test/BpTreeTest.cs
+++ src/SkipList.Core.Test/BpTreeTest.cs
@@ -1,8 +1,6 @@
 ﻿namespace BpTree.Core.Test
 {
     using System;
-    using System.Collections.Generic;
-    using System.Diagnostics;
     using NUnit.Framework;
 
     [TestFixture]
@@ -101,54 +99,6 @@
             }
         }
 
-        [Test]
-        [Ignore]
-        public void Performance_Add_1()
-        {
-#if (DEBUG)
-            Console.WriteLine("Performance tests should be run only in Release mode!");
-#endif
-            var n = 500000;
-            var items = GenerateItems(n);
 
-            { 
-                var skipList = new SkipList<int, int>();
-                var timer = Stopwatch.StartNew();
-                for (var index = 0; index < n; index++)
-                {
-                    skipList[items[index]] = items[index];
-                }
-                Console.WriteLine(timer.Elapsed);
-            }
-
-//            { 
-//                var dictionary = new SortedDictionary<int, int>();
-//                var timer = Stopwatch.StartNew();
-//                for (var index = 0; index < n; index++)
-//                {
-//                    dictionary[items[index]] = items[index];
-//                }
-//                Console.WriteLine(timer.Elapsed);
-//            }
-        }
-
-        private static int[] GenerateItems(int n)
-        {
-            var random = new Random();
-            var itemSet = new HashSet<int>();
-            var items = new int[n];
-            var i = 0;
-            while (i < n)
-            {
-                var value = random.Next();
-                if (itemSet.Contains(n))
-                {
-                    continue;
-                }
-                items[i] = value;
-                i++;
-            }
-            return items;
-        }
     }
 }
