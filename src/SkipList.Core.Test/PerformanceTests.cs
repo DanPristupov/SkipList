2a149e7fb10efba2f2dd984d0e449e801aa7b58a
Dan Pristupov <danil@danil.cz>
2015-05-04 22:56:02 +0200

    Implements Performance_Search_1 performance test.

--- src/SkipList.Core.Test/PerformanceTests.cs
+++ src/SkipList.Core.Test/PerformanceTests.cs
@@ -11,6 +11,55 @@
     {
         [Test]
         [Ignore]
+        public void Performance_Search_1()
+        {
+#if (DEBUG)
+            Console.WriteLine("Performance tests should be run only in Release mode!");
+#endif
+            // todo: implement performance test for multiple N
+            var n = 500000;
+            var items = GenerateItems(n);
+            var rnd = new Random();
+
+            var testGroup = new TestGroup("Performance_Search_1");
+
+            {
+                var skipList = new SkipList<int, int>();
+                for (var index = 0; index < n; index++)
+                {
+                    skipList[items[index]] = items[index];
+                }
+
+                var testResultSummary = testGroup.PlanAndExecute("Performance_Search_" + n, () =>
+                {
+                    for (var i = 0; i < n; i++)
+                    {
+                        var value = skipList[items[rnd.Next(n)]];
+                    }
+                }, 5);
+                Console.WriteLine(testResultSummary);
+            }
+
+            {
+                var dictionary = new SortedDictionary<int, int>();
+                for (var index = 0; index < n; index++)
+                {
+                    dictionary[items[index]] = items[index];
+                }
+                var clrResultSummary = testGroup.PlanAndExecute("Performance_Search_CLR" + n, () =>
+                {
+                    for (var i = 0; i < n; i++)
+                    {
+                        var value = dictionary[items[rnd.Next(n)]];
+                    }
+                }, 5);
+
+                Console.WriteLine(clrResultSummary);
+            }
+        }
+
+        [Test]
+        [Ignore]
         public void Performance_Add_1()
         {
 #if (DEBUG)
