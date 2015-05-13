b2dc499f24c5bdcd49796481b0cfbb8585d3ce41
Dan Pristupov <danil@danil.cz>
2015-05-13 22:10:09 +0200

    Implements global search performance test.

--- src/SkipList.Core.Test/PerformanceTests.cs
+++ src/SkipList.Core.Test/PerformanceTests.cs
@@ -2,12 +2,19 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Text;
     using NUnit.Framework;
     using SimpleSpeedTester.Core;
 
     [TestFixture]
     public class PerformanceTests
     {
+        private const string Iterations = "Iterations";
+        private const string SkipList = "SkipList";
+        private const string CsrDictionary = "ClrDictionary";
+
+        private const int NumberOfIterations = 10000;
+
         [Test]
         [Ignore]
         public void Performance_Search_1()
@@ -15,46 +22,52 @@
 #if (DEBUG)
             Console.WriteLine("Performance tests should be run only in Release mode!");
 #endif
-            // todo: implement performance test for multiple N
-            var n = 500000;
-            var items = GenerateItems(n);
-            var rnd = new Random();
+            var numberOfItemsList = new[] {1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000, 256000, 512000};
+            var result = CreateResultDictionary();
 
-            var testGroup = new TestGroup("Performance_Search_1");
-
+            foreach (var n in numberOfItemsList)
             {
-                var skipList = new SkipListDictionary<int, int>();
-                for (var index = 0; index < n; index++)
+                var items = GenerateItems(n);
+                var rnd = new Random();
+                result[Iterations].Add(n.ToString());
+
+                var testGroup = new TestGroup("Performance_Search_1");
+
                 {
-                    skipList[items[index]] = items[index];
+                    var skipList = new SkipListDictionary<int, int>();
+                    for (var index = 0; index < n; index++)
+                    {
+                        skipList[items[index]] = items[index];
+                    }
+
+                    var testResultSummary = testGroup.PlanAndExecute("Performance_Search_" + n, () =>
+                    {
+                        for (var i = 0; i < NumberOfIterations; i++)
+                        {
+                            var value = skipList[items[rnd.Next(n)]];
+                        }
+                    }, 5);
+                    result[SkipList].Add(testResultSummary.AverageExecutionTime.ToString("F1"));
                 }
 
-                var testResultSummary = testGroup.PlanAndExecute("Performance_Search_" + n, () =>
                 {
-                    for (var i = 0; i < n; i++)
+                    var dictionary = new SortedDictionary<int, int>();
+                    for (var index = 0; index < n; index++)
                     {
-                        var value = skipList[items[rnd.Next(n)]];
+                        dictionary[items[index]] = items[index];
                     }
-                }, 5);
-                Console.WriteLine(testResultSummary);
-            }
+                    var clrResultSummary = testGroup.PlanAndExecute("Performance_Search_CLR" + n, () =>
+                    {
+                        for (var i = 0; i < NumberOfIterations; i++)
+                        {
+                            var value = dictionary[items[rnd.Next(n)]];
+                        }
+                    }, 5);
 
-            {
-                var dictionary = new SortedDictionary<int, int>();
-                for (var index = 0; index < n; index++)
-                {
-                    dictionary[items[index]] = items[index];
+                    result[CsrDictionary].Add(clrResultSummary.AverageExecutionTime.ToString("F1"));
                 }
-                var clrResultSummary = testGroup.PlanAndExecute("Performance_Search_CLR" + n, () =>
-                {
-                    for (var i = 0; i < n; i++)
-                    {
-                        var value = dictionary[items[rnd.Next(n)]];
-                    }
-                }, 5);
-
-                Console.WriteLine(clrResultSummary);
             }
+            PrintResult(result, "Search");
         }
 
         [Test]
@@ -108,6 +121,46 @@
                 i++;
             }
             return items;
+        }
+
+        private static Dictionary<string, IList<string>> CreateResultDictionary()
+        {
+            var result = new Dictionary<string, IList<string>>();
+            result[Iterations] = new List<string>();
+            result[SkipList] = new List<string>();
+            result[CsrDictionary] = new List<string>();
+            return result;
+        }
+
+        private void PrintResult(Dictionary<string, IList<string>> result, string testName)
+        {
+            PrintHeaderRow(result, testName);
+            PrintResultRow(result, SkipList);
+            PrintResultRow(result, CsrDictionary);
+        }
+
+        private void PrintHeaderRow(Dictionary<string, IList<string>> result, string testName)
+        {
+            var sb = new StringBuilder();
+            sb.Append(testName);
+            
+            foreach (var testResult in result[Iterations])
+            {
+                sb.Append("\t" + testResult);
+            }
+            Console.WriteLine(sb.ToString());
+        }
+
+        private void PrintResultRow(Dictionary<string, IList<string>> result, string groupName)
+        {
+            var sb = new StringBuilder();
+            sb.Append(groupName);
+
+            foreach (var testResult in result[groupName])
+            {
+                sb.Append("\t" + testResult);
+            }
+            Console.WriteLine(sb.ToString());
         }
     }
 }
